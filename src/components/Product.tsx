ae4123966258440f124fff586b6f5affdb8cbe1e
Larionov-Anton <larionovanton05@gmail.com>
2022-08-01 16:56:57 +0300

    Add components,useState,loop for render components

---
+++ src/components/Product.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import {IProduct} from '../models'
+
+interface ProductProps {
+	product: IProduct
+}
+
+export function Product({product}: ProductProps) {
+
+	// Хуки состояния
+	const [details, setDetails] = useState(false)
+
+	const btnBgClasses = details ? 'bg-yellow-400' : 'bg-blue-400'
+	const btnClasses = ['py-2 px-4 border', btnBgClasses]
+
+	return (
+		<div
+		className='border py-2 px-4 rounded flex flex-col items-center mb-2 cla="true" '>
+			<img src={product.image} alt={product.title} className='w-1/6' />
+			<p>{product.title}</p>
+			<p className='font-bold'>{product.price}</p>
+			<button 
+				className = {btnClasses.join(' ')}
+				onClick={() => setDetails(prev => !prev)}
+				>
+					{details ? 'Hide Details' : 'Show Details'}
+			</button>
+
+			{details && <div>
+				<p>{product.description}</p>
+				<p>Rate: <span style={{ fontWeight: 'bold' }}>{product.rating.rate}</span></p>
+			</div>}
+		</div>
+	)
+}
